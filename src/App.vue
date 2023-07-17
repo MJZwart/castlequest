dcfcc24d66f36aeb159c6bd6f15ece7aef856d4f
MJZwart <prorenata.blue@gmail.com>
2023-07-17 15:02:22 +0200

    Basic website with quests

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,35 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="content">
+    <header>
+      <div class="header">
+        <h1 class="gradient">Castlequest</h1>
+      </div>
+    </header>
+    <main>
+      <div class="active-quest">{{ activeQuest }}</div>
+      <button class="random-quest-btn btn" @click="getRandomQuest">Generate new quest</button>
+    </main>
+    <footer>
+      <div class="footer">
+        Took a picture of your quest? Share them with the hashtag
+        <span class="gradient">#castlequest</span> on social media!
+      </div>
+    </footer>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { quests } from './quests'
+
+const activeQuest = ref<string | null>(null)
+
+const getRandomQuest = () => {
+  const randomNumber = Math.floor(Math.random() * quests.length)
+  activeQuest.value = quests[randomNumber]
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+onMounted(() => {
+  getRandomQuest()
+})
+</script>
